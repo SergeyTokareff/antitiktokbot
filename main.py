9e7451425e311adeb2d89c47a6362f3b486658d3
SergeyTokareff <sergey.v.tokareff@gmail.com>
2021-01-24 01:02:33 +0200

    'deploy'

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+bot = telebot.TeleBot('1461284843:AAHA115L3Tv3mqd1SV9IscI7AkumfgVYu2s')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    res = message.text.find('tiktok')
+
+    if res > -1:
+        bot.delete_message(message.chat.id, message.message_id)
+
+bot.polling(none_stop=True, interval=0)
